9ca9b50714040c770ee8bc5f2477b93113e8a7e1
DJK101 <danieljeffersonkennedy@gmail.com>
2024-12-03 13:41:36 +0000

    Remove redundant code

--- app.py
+++ app.py
@@ -6,18 +6,20 @@ import datetime
 from lib.block_timer import BlockTimer
 from lib.data_snapshot import DataSnapshot
 
+logger = logging.getLogger(__name__)
+
 app = Flask(__name__)
 
-# Configure caching
-app.config["CACHE_TYPE"] = "SimpleCache"  # Use in-memory cache
-app.config["CACHE_DEFAULT_TIMEOUT"] = 10  # Default timeout: 300 seconds
-cache = Cache(app)
 
-# Global instance of DataSnapshot
-data_snapshot = DataSnapshot()
+@app.route("/")
+def index():
+    with BlockTimer():
+        logging.info("Test message, 10,000 for loop")
+        for i in range(10000):
+            x = i + 1
+    return {"status": "success", "message": "Hello, World!"}, 200
 
 
-# Load configuration from config.json
 def load_config():
     try:
         with open("config.json") as config_file:
@@ -28,50 +30,7 @@ def load_config():
         config = {}
     return config
 
-
-# Basic setup for logging
-def init_config_logging():
-    logging.basicConfig(
-        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-    )
-    logging.info("Configuration and logging initialized")
-
-
-# Endpoint for /hello
-@app.route("/hello")
-def hello():
-    with BlockTimer("Hello Handler Timer") as timer:
-        # This is where you would handle the request logic
-        response = "Hello World!"
-    # Record metrics for this request
-    data_snapshot.record_request(timer.elapsed)
-    return response, 200
-
-
-# New /metrics handler
-@app.route("/metrics")
-def metrics():
-    """Return metrics in JSON format."""
-    return jsonify(data_snapshot.get_metrics())
-
-
-@app.route("/test")
-def test():
-    cache_key = "system_metrics"
-
-    if (result := cache.get(cache_key)) is None:
-        result = data_snapshot.gather_system_metrics().__dict__
-        cache.set(cache_key, result, timeout=10)
-
-    return {
-        "timestamp": datetime.datetime.now().isoformat(),
-        "status": "success",
-        "content": result,
-    }
-
-
 if __name__ == "__main__":
-    init_config_logging()
     config = load_config()
     port = config.get("port", 5000)
     app.run(host="0.0.0.0", port=port)
